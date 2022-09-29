8051aa808010b599e7752340abc53622eaf04c68
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-09-28 22:28:18 -0300

    feat: change appContainer style

--- src/components/layout/AppContainer.tsx
+++ src/components/layout/AppContainer.tsx
@@ -1,13 +1,8 @@
-import { Container, styled } from "@mui/material";
+import { Container } from "@mui/material";
 import { FunctionComponent, PropsWithChildren } from "react";
-
-const StyledContainer = styled(Container)({
-  height: "100vh",
-  width: "100vw",
-});
 
 export const AppContainer: FunctionComponent<PropsWithChildren> = ({
   children,
 }) => {
-  return <StyledContainer>{children}</StyledContainer>;
+  return <Container>{children}</Container>;
 };

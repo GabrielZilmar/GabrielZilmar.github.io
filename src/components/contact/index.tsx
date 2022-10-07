bd8a0e6a3eda6ba7e99182a59ed0b220a176ae7a
Gabriel Zilmar <gabrielzilmar@hotmail.com>
2022-10-06 22:31:34 -0300

    feat: add contact page

---
+++ src/components/contact/index.tsx
@@ -0,0 +1,7 @@
+import ContainerLayout from "~/components/layout/Container/Container";
+
+const Contact: React.FC = () => {
+  return <ContainerLayout id="contact">Contact</ContainerLayout>;
+};
+
+export default Contact;
